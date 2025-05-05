27cd7b9620d628df52645b9d1a546964b91fd908
FarrelDinarta <farrel.dinarta@ruparupa.com>
2025-05-05 15:22:40 +0700

    fix

--- app/service/inference_service.py
+++ app/service/inference_service.py
@@ -1,5 +1,7 @@
-import io
+import os
+from pydub import AudioSegment
 from fastapi import Depends
+import tempfile
 from app.dto.inference_dto import GenerateTextInferenceRequest, GenerateTextInferenceResponse
 from app.provider.llm.base import LLM, get_llm
 from app.exception.http_exception import WebSocketAPIException
@@ -18,15 +20,30 @@ class InferenceService:
             output_lines.append("[%.2fs -> %.2fs] %s" % (word.start, word.end, word.word))
 
     return "\n".join(output_lines)
+  
+  def produce_audio_file(self, audio: bytes) -> str:
+    audio_segment = AudioSegment(data=audio, frame_rate=16000, sample_width=2, channels=1)
+
+    with tempfile.NamedTemporaryFile(delete=False, suffix=".mp3") as temp_file:
+        temp_path = temp_file.name
+        audio_segment.export(temp_path, format="mp3", bitrate="128k")
+    
+    return temp_path
 
   async def generate(self, payload : GenerateTextInferenceRequest) -> GenerateTextInferenceResponse:
-    segments : list = await self.__llm.get_whisperx_model().inference(payload.audio)
+
+    audio = self.produce_audio_file(payload.audio)
+
+    segments : list = await self.__llm.get_whisperx_model().inference(audio)
     if not segments:
       raise WebSocketAPIException(
         status=400,
         message="Failed to transcribe audio",
         error="Failed to transcribe audio"
       )
+    
+    if os.path.exists(audio):
+      os.remove(audio)    
     
     merged_text = self.segments_to_string(segments)
     return GenerateTextInferenceResponse(text=merged_text)
